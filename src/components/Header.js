42312d1ce1cceba4ee141589a2715a8816d99079
marisasaunders <marisaksaunders@gmail.com>
2022-03-13 23:13:34 -0400

    all tests passed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Header(props) {
+function Header({name}) {
   return (
-    <>
-      <h1>{props.name}</h1>
-    </>
+    <header>
+      <h1>{name}</h1>
+    </header> 
   )
 }
 
